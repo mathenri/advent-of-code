0b8f0073a79963f58819df77578e6fec2185d0da
Mattias Henriksson <henriksson.mj@gmail.com>
2020-10-31 14:43:19 +0100

    Clean up part 1 day 1 2019

--- 2019/1/part1/main.go
+++ 2019/1/part1/main.go
@@ -1,28 +1,33 @@
 package main
 
-import "fmt"
-import "io/ioutil"
-import "strings"
-import "strconv"
-import "math"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"strconv"
+	"math"
+	"log"
+)
 
 func main() {
 	
+	// read input file
 	data, err := ioutil.ReadFile("input.txt")
+	handleError(err)
+	
+	// calculate fuel needed
+	var totalFuel float64 = 0
+	for _, line := range strings.Split(string(data), "\n") {
+		mass, err := strconv.Atoi(line)
+		handleError(err)
+		fuel := math.Floor(float64(mass / 3.0)) - 2
+		totalFuel += fuel
+	}
+	fmt.Printf("Total fuel %f", totalFuel)
+}
 
+func handleError(err error) {
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		var totalFuel float64 = 0
-		for _, line := range strings.Split(string(data), "\n") {
-			mass, err := strconv.Atoi(line)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fuel := math.Floor(float64(mass / 3.0)) - 2
-				totalFuel += fuel
-			}
-		}
-		fmt.Printf("Total fuel %f", totalFuel)
+		log.Fatal(err)
 	}
 }
